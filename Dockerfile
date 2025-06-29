d28f483e982d01d47ce1bb34cbf8d1f8880b9057
andrespaida <jordanpaida.bj@hotmail.com>
2025-06-28 20:57:04 -0500

    deploy

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,15 @@
-# Dockerfile
-
 FROM golang:1.23
 
-# Create working directory
 WORKDIR /app
 
-# Copy dependency file
 COPY go.mod ./
 COPY go.sum ./
 RUN go mod download
 
-# Copy the rest of the code
 COPY . .
 
-# Build the app
 RUN go build -o main .
 
-# Expose the port
 EXPOSE 4001
 
-# Run the app
 CMD ["./main"]
--- main.go
+++ main.go
@@ -12,35 +12,28 @@ import (
 )
 
 func main() {
-	// Cargar archivo .env
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("No .env file found")
 	}
 
-	// Conexión a la base de datos
 	database.ConnectDB()
 
-	// Inicializar router Gin
 	router := gin.Default()
 
-	// Habilitar CORS globalmente
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"}, // o reemplaza con []string{"http://98.85.86.231"} para mayor seguridad
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type"},
 		AllowCredentials: true,
 	}))
 
-	// Ruta principal
 	router.GET("/products", handlers.ReadProducts)
 
-	// Puerto desde .env o por defecto 4001
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "4001"
 	}
 
-	// Iniciar servidor
 	router.Run(":" + port)
 }
