20ad336b373b17981abefe850b264ccf9cdeb736
andrespaida <jordanpaida.bj@hotmail.com>
2025-06-30 21:26:23 -0500

    change id object

--- models/product.go
+++ models/product.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Product struct {
-    ID          primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+    ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
     Name        string             `json:"name" bson:"name"`
     Description string             `json:"description" bson:"description"`
     Price       float64            `json:"price" bson:"price"`
